b7ae1cc77cbea97d1b92d7c2a8eb9181ef132dae
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-12-05 17:27:45 +0530

    mongodb setup

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,54 +1,32 @@
-const { readData, writeData } = require('../utils/fileHelper.js');
-const { v4: uuidv4 } = require('uuid');
+const Task = require('./Task');
 
 class TaskModel {
   static async findAll() {
-    const tasks = await readData();
+    const tasks = await Task.find(); 
     return tasks;
   }
 
   static async findById(id) {
-    const tasks = await readData();
-    return tasks.find(t => t.id === id);
+    return Task.findById(id);
   }
 
   static async create(taskData) {
-    const tasks = await readData();
-    const newTask = {
-      id: uuidv4(),
-      ...taskData,
-      completed: false,
-      createdAt: new Date().toISOString()
-    };
-
-    tasks.push(newTask);
-    await writeData(tasks);
+    const newTask = await Task.create(taskData); 
     return newTask;
   }
 
-  static async update(id, taskData){
-    const tasks = await readData();
-    const index = tasks.findIndex(t => t.id === id);
-
-    if(index == -1) {
-      tasks[index] = {...tasks[index], ...tasksData};
-      await writeData();
-      return tasks[index];
-    }
-
-    return null;
+  static async update(id, taskData) {
+    const updatedTask = await Task.findByIdAndUpdate(
+        id, 
+        taskData, 
+        { new: true, runValidators: true }
+    );
+    return updatedTask; 
   }
 
   static async remove(id) {
-    const tasks = await readData();
-    const filteredTasks = tasks.filter(t => t.id != id);
-
-    if(filteredTasks.length != tasks.length) {
-      await writeData(filteredTasks);
-      return true;
-    }
-
-    return false;
+    const result = await Task.findByIdAndDelete(id); 
+    return !!result; 
   }
 }
 
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 const taskRoutes = require('./routes/taskRoutes');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err.message));
 
 app.use(cors());
 app.use(express.json());
